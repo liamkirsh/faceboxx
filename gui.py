dafbdb594d6e2b32b456f7f14a084b06e60ab810
Girish Kowligi <gkowligi@umail.ucsb.edu>
2015-02-01 07:28:16 -0800

    Added splash.py into gui.py
    Added splash.py into gui.py, and removed splash.py

--- gui.py
+++ gui.py
@@ -2,6 +2,7 @@ import wx
 
 from Tkinter import *
 from tkFileDialog import askopenfilename
+import Tkinter as tk
 
 
 
@@ -24,7 +25,27 @@ class windowClass(wx.Frame):
         print(filename)
 
     def basicGUI(self):
+        
+        #splash screen
+        root = tk.Tk()
 
+        root.overrideredirect(True)
+        width = root.winfo_screenwidth()
+        height = root.winfo_screenheight()
+        root.geometry('%dx%d+%d+%d' % (width*0.5, height*0.5, width*0.1, height*0.1))
+
+        image_file = "faceboxx_logo.gif"
+
+        image = tk.PhotoImage(file=image_file)
+        canvas = tk.Canvas(root, height=height*0.8, width=width*0.8, bg="black")
+        canvas.create_image(width*0.5/2, height*0.5/2, image=image)
+        canvas.pack()
+
+        # show the splash screen for 3000 milliseconds then destroy
+        root.after(3000, root.destroy)
+        root.mainloop()
+
+        #start application
         panel = wx.Panel(self)
 
         
